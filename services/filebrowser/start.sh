c018a788fa74c29b166a21e02d7d96cc5f1b8e4a
Luandro <luandro@digital-democracy.org>
2023-03-27 13:17:49 -0300

    fix: simplify filebrowser

--- services/filebrowser/start.sh
+++ services/filebrowser/start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
 echo "--------- Starting FileBrowser -----------"
-echo "Initiating config"
-filebrowser config init
-echo "Loading config file"
-echo "LOCALE: $LOCALE"
-sed -i -r "s/#LOCALE/$LOCALE/g" /config/custom/filebrowser.json
-echo "TITLE: $TITLE"
-sed -i -r "s/#TITLE/$TITLE/g" /config/custom/filebrowser.json
-filebrowser config import /config/custom/filebrowser.json
 filebrowser config set --branding.files "/config/custom/branding"
 filebrowser
