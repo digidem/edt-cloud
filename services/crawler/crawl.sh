f0ae3a3df13dd0fd391464f27c4bb1b4d65190a3
Luandro <luandro@digital-democracy.org>
2023-04-23 12:32:18 -0300

    chore: disable crawl for now

--- services/crawler/crawl.sh
+++ services/crawler/crawl.sh
@@ -5,20 +5,20 @@ mkdir -p /app/content/installers
 rm -f /app/content/installers/*
 app-crawler --path=/app/content/installers
 echo "Crawling installers done"
-echo "Crawling content!"
-crawl --config /app/crawl-config.yml --pageTimeout 180000
-echo "Crawling done"
-count=$(ls -1 /crawls/collections/crawl-*/archive/*.warc.gz 2>/dev/null | wc -l)
-if [ $count != 0 ]; then
-echo "Cleaning old-websites folder"
-rm -rf /app/content/old-websites
-mkdir -p /app/content/old-websites
-mkdir -p /app/content/offline-websites/
-echo "Moving old content"
-mv /app/content/offline-websites/* /app/content/old-websites/
-echo "Copying content to content folder"
-mv /crawls/collections/crawl-*/archive/* /app/content/offline-websites/
-echo "Moved!"
-fi
+# echo "Crawling content!"
+# crawl --config /app/crawl-config.yml --pageTimeout 180000
+# echo "Crawling done"
+# count=$(ls -1 /crawls/collections/crawl-*/archive/*.warc.gz 2>/dev/null | wc -l)
+# if [ $count != 0 ]; then
+# echo "Cleaning old-websites folder"
+# rm -rf /app/content/old-websites
+# mkdir -p /app/content/old-websites
+# mkdir -p /app/content/offline-websites/
+# echo "Moving old content"
+# mv /app/content/offline-websites/* /app/content/old-websites/
+# echo "Copying content to content folder"
+# mv /crawls/collections/crawl-*/archive/* /app/content/offline-websites/
+# echo "Moved!"
+# fi
 
 echo "Sleeping for $CRAWL_TIMER seconds..."
